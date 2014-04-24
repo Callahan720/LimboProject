3fea2045b7155f4ed1680c5d39be448b6cc19965
Callahan720 <callahan720@gmail.com>
2014-04-24 16:52:11 -0400

    final alpha insert sql included

---
+++ tempinsertstuff.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO stuff (item_name, description, location_id, room, contact_name, email, phone_number, status)
+VALUES  ( "Test Name", "Test Desc", 2, "2023", "John Smith", "jsmith@email.com", "555-5678", "found" ),
+        ( "Test Name1", "Test Desc1", 4, "HC 2023", "John Smith", "jsmith@email.com", "555-5678", "found" ),
+        ( "Test Name2", "Test Desc2", 5, "LT2023", "John Smith", "jsmith@email.com", "555-5678", "found" ),
+
+        ( "Test Name3", "Test Desc3", 6, "4", "John Smith", "jsmith@email.com", "555-5678", "lost" ),
+        ( "Test Name4", "Test Desc4", 12, "99", "John Smith", "jsmith@email.com", "555-5678", "lost" ),
+        ( "Test Name5", "Test Desc5", 11, "34", "John Smith", "jsmith@email.com", "555-5678", "lost" );
