fae359a7c3191093cd9b9d44a290e66b454c9da2
ricknussell31 <ricknussell31@gmail.com>
2014-04-23 20:24:09 -0400

    Added foundform, helpers and limbo

---
+++ foundform.php
@@ -0,0 +1,125 @@
+<!--
+Authors Kevin Callahan and Nick Russell
+-->
+<!DOCTYPE html>
+<html>
+<?php
+# Connect to MySQL server and the database
+require( 'includes/connect_db.php' ) ;
+
+# Includes these helper functions
+require( 'includes/helpers.php' ) ;
+
+//Initialize president info on a GET
+if ($_SERVER[ 'REQUEST_METHOD' ] == 'GET') {
+	$status = "lost";
+	$item_name = "" ;
+    $description = "";
+	$location_id = "" ;
+	$room = "" ;
+	$contact_id = "" ;
+	$email = "";
+	$phone_number = "" ;
+	
+}
+if ($_SERVER[ 'REQUEST_METHOD' ] == 'POST') {
+	
+	$status = $_POST['status'];
+
+	$item_name = $_POST['item_name'] ;
+
+    $description = $_POST['description'] ;
+	
+	$location_id = $_POST['location_id'] ;
+	
+	$room = $_POST['room'] ;
+	
+	$contact_id = $_POST['contact_id'] ;
+	
+	$email = $_POST['email'] ;
+	
+	$phone_number = $_POST['phone_number'] ;
+	
+	if (!valid_name($item_name)) 
+		echo '<p style="color:red">Please give a valid item name. </p>' ;
+	else if (!valid_name($location_id)) 
+		echo '<p style="color:red">Please give a valid location. </p>' ;
+	else if (!valid_name($contact_id)) 
+		echo '<p style="color:red">Please give a valid name for your contact information. </p>' ;
+	else if ((!filter_var($email, FILTER_VALIDATE_EMAIL)) && (!valid_number($phone_number))) 
+		echo '<p style="color:red">Please give either a valid email address or phone number. </p>' ;
+	else 
+		$results = insert_record($status, $item_name, $description, $location_id, $room, $contact_id, $email, $phone_number) ;
+}
+else if($_SERVER[ 'REQUEST_METHOD' ] == 'GET') {
+	if (isset($_GET['id']))
+		show_record($dbc, $_GET['id']);
+}
+
+# Show the records
+show_link_records($dbc);
+
+# Close the connection
+mysqli_close($dbc);
+
+show_form($status, $item_name, $description, $location_id, $room, $contact_id, $email, $phone_number) ;
+
+function show_form($status, $item_name, $description, $location_id, $room, $contact_id, $email, $phone_number) {
+	# Get inputs from the user.
+	echo '<h1> Lost and Found Stuff </h1>';
+	echo '<form action="foundform.php" method="POST">';
+	echo '<table>';
+	echo '<tr>';
+	echo '<td>I have</td> 
+		  <td colspan = "2"><select name = "status">
+			  <option value = "lost">Lost</option>;
+			  <option value = "found">Found</option>
+			  </select>
+			  </td>
+		  <td> an item </td>';
+	echo '</tr>';
+	echo '<tr>';
+	echo '<td> Name of Item:* </td> <td colspan = "3"><input type="text" name="item_name" value = "' . $item_name . '"></td>';
+	echo '</tr>';
+	echo '<tr>';
+	echo '<td>Location of Item: *:</td> 
+		  <td><select name = "location_id">
+			  <option value = "Champagnat">Champagnat</option>;
+			  <option value = "Leo">Leo</option>
+			  <option value = "Sheahan">Sheahan</option>
+			  <option value = "Marian">Marian</option>
+			  <option value = "Gartland">Gartland</option>
+			  <option value = "Midrise">Midrise</option>
+			  <option value = "Upper West Cedar">Upper West Cedar</option>
+			  <option value = "Talmadge">Talmadge</option>
+			  <option value = "Lower Fulton">Lower Fulton</option>
+			  <option value = "Upper Fulton">Upper Fulton</option>
+			  <option value = "Middle Fulton">Middle Fulton</option>
+			  <option value = "Foy Townhouses">Foy Townhouses</option>
+			  <option value = "Tennis Courts">Tennis Courts</option>
+			  <option value = "Greystone">Greystone</option>
+			  <option value = "Tenney Stadium">Tenney Stadium</option>
+			  <option value = "St. Anne">St. Anne</option>
+			  <option value = "Cornell Boathouse">Cornell Boathouse</option>
+			  <option value = "Marist Boathouse">Marist Boathouse</option>
+			  <option value = "Student Center">Student Center</option>
+			  <option value = "Music Building">Music Building</option>
+			  <option value = "Hancock">Hancock</option>
+			  <option value = "Donnelly">Donnelly</option>
+			  <option value = "Dyson">Dyson</option>
+			  <option value = "Fontaine">Fontaine</option>
+			  <option value = "Fontaine Annex">Fontaine Annex</option>
+			  <option value = "Lowell Thomas">Lowell Thomas</option>
+			  <option value = "Donnelly">Donnelly</option>
+			  <option value = "Donnelly">Donnelly</option>
+			  <option value = "Donnelly">Donnelly</option>
+			  <option value = "Donnelly">Donnelly</option>
+			  </select></td>';
+	echo '</tr>';
+	echo '</table>';
+	echo '<p><input type="submit" ></p';
+	echo '</form>';
+	echo '</html>';
+}
+?>
+
---
+++ limbo.sql
@@ -0,0 +1,102 @@
+# This file creates and populates the Limbo database.
+# Author: Kevin Callahan and Nicholas Russell
+#Version 3.0
+
+#Creates limbo database.
+CREATE database IF NOT EXISTS limbo_db ;
+USE limbo_db;
+
+#Creates table users.
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
+user_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+user_name TEXT NOT NULL ,
+pass TEXT NOT NULL
+);
+
+#Inserts data into users.
+INSERT INTO users (user_name, pass)
+VALUES ("admin", "gaze11e");
+
+#Creates table stuff.
+DROP TABLE IF EXISTS stuff;
+CREATE TABLE IF NOT EXISTS stuff (
+  id  INT PRIMARY KEY AUTO_INCREMENT ,
+  item_name TEXT NOT NULL, 
+  description  TEXT,
+  create_date DATETIME DEFAULT NOW(),
+  update_date DATETIME DEFAULT NOW(),
+  location_id INT NOT NULL,
+  room TEXT,
+  contact_name TEXT,
+  email TEXT,
+  phone_number TEXT, 
+  status SET("found", "lost", "claimed") NOT NULL
+ ) ;
+
+
+#Creates table locations.
+DROP TABLE IF EXISTS locations;
+CREATE TABLE IF NOT EXISTS locations (
+id INT PRIMARY KEY AUTO_INCREMENT,
+create_date DATETIME DEFAULT NOW(),
+update_date DATETIME DEFAULT NOW(),
+name TEXT NOT NULL,
+location_type SET ("Building", "Residence Hall", "Other") NOT NULL
+);
+
+
+#Inserts data into locations.
+INSERT INTO locations (name, location_type)
+VALUES 
+(  "Champagnat", "Residence Hall"),
+(  "Leo", "Residence Hall"), 
+(  "Sheahan", "Residence Hall"), 
+(  "Marian", "Residence Hall"), 
+(  "Gartland", "Residence Hall"), 
+(  "Midrise", "Residence Hall"), 
+(  "Upper West Cedar", "Residence Hall"), 
+(  "Lower West Cedar", "Residence Hall"), 
+(  "Talmadge Court", "Residence Hall"), 
+(  "Upper New Townhouses", "Residence Hall"), 
+(  "Lower New Townhouses", "Residence Hall"), 
+(  "Lower Fulton", "Residence Hall"),
+(  "Upper Fulton", "Residence Hall"),
+(  "Middle Fulton", "Residence Hall"),
+(  "Tennis Courts", "Other"),
+(  "Foy Townhouses", "Residence Hall"), 
+(  "Greystone", "Building"), 
+(  "Tenney Stadium", "Other"), 
+(  "St. Anne's", "Other"), 
+(  "Cornell Boathouse", "Other"),
+(  "Marist Boathouse", "Other"), 
+(  "Student Center", "Building"), 
+(  "Music Building", "Building"), 
+(  "Jazzman's Cafe", "Building"), 
+(  "Main Cafeteria", "Building"),
+(  "McCann", "Building"), 
+(  "Donnelly", "Building"), 
+(  "Dyson", "Building"), 
+(  "Fontaine", "Building"), 
+(  "Fontaine Annex", "Building"),
+(  "Lowell Thomas", "Building"), 
+(  "Library", "Building"), 
+(  "Hancock", "Building"), 
+(  "Steel Plant", "Building"); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
